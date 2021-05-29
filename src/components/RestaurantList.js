3487bd42e5037e2e7f57e292e9f4a2199f38f9ab
Ami Onodera <onodera.camila@gmail.com>
2021-05-29 17:32:34 +0200

    wip

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,24 +1,38 @@
 import { useEffect, useState } from "react";
 
+const apiUrl = "https://redi-final-restaurants.herokuapp.com/restaurants";
+
 export default function RestaurantList() {
-	const [name, setName] = useState("");
+	let [name, setName] = useState("");
 
 	useEffect(() => {
 		const getName = async () => {
-			const response = await fetch(
-				`https://redi-final-restaurants.herokuapp.com/restaurants`
-			);
+			const response = await fetch(apiUrl);
 			const data = await response.json();
-			const { name: _name } = data;
-			setName(_name);
-			console.log(data);
+			// const { name: _name } = data.results;
+			// setName(_name);
+			name = data.results[0].name;
+			setName(name);
+			console.log(name);
 		};
 		getName();
 	});
 
 	return (
-		<div className="App">
+		<div>
 			<h1>{name}</h1>
 		</div>
 	);
+
+	// const restaurantNames = name.map(({ name }) => name);
+
+	// 	return (
+	// 		<div>
+	// 			<ul>
+	// 				{restaurantNames.map((item) => (
+	// 					<li>{item}</li>
+	// 				))}
+	// 			</ul>
+	// 		</div>
+	// 	);
 }
