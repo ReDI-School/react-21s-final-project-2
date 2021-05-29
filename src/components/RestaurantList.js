93d3633150a9bcd5f34f311b77c94e6137cdbba4
Ami Onodera <onodera.camila@gmail.com>
2021-05-29 19:43:14 +0200

    fist task OK

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import "./restaurantList.css";
 
 const apiUrl = "https://redi-final-restaurants.herokuapp.com/restaurants";
 
@@ -11,17 +12,29 @@ export default function RestaurantList() {
 			const data = await response.json();
 
 			setRestaurantName(data.results);
+			console.log(data.results);
 		};
 		getName();
 	}, []);
 
 	return (
-		<div>
-			<ul>
+		<div className="pageContainer">
+			<div className="restList">
 				{restaurantName.map((restaurant) => (
-					<li key={restaurant.id}>{restaurant.name}</li>
+					<div className="listItem" key={restaurant.id}>
+						<img src={restaurant.photos[0].links[0]}></img>
+						<h1>{restaurant.name}</h1>
+						<p>Cuisine: {restaurant.cuisine}</p>
+						<p>
+							{restaurant.opening_hours.open_now ? (
+								<p className="open">Open</p>
+							) : (
+								<p className="closed">Closed</p>
+							)}
+						</p>
+					</div>
 				))}
-			</ul>
+			</div>
 		</div>
 	);
 }
