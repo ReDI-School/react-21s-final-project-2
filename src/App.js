3caa79f59a813a5ea351d0bec4975efe9e92717b
AmirMoltames <amir.moltames@gmail.com>
2021-05-31 19:42:22 +0200

    React-Router-Implementation Simple NavBar with Static Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import logo from "./logo.svg";
 import "./App.css";
+import NavBar from "./Components/NavBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./Components/Layout/Home";
+import { About } from "./Components/Layout/About";
+import { Contact } from "./Components/Layout/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <>
+      <Router>
+        <NavBar />
+
+        <div className="pages">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
