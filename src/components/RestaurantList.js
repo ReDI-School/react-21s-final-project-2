31e624d1aa81275b49a31275f996437cd5d40050
Ami Onodera <onodera.camila@gmail.com>
2021-06-08 17:33:30 +0200

    horizontal cards showing all info requested and in light colors

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -22,16 +22,40 @@ export default function RestaurantList() {
             <div className="restList">
                 {restaurantName.map((restaurant) => (
                     <div className="listItem" key={restaurant.id}>
-                        <img src={restaurant.photos[0].links[0]}></img>
-                        <h1>{restaurant.name}</h1>
-                        <p>Cuisine: {restaurant.cuisine}</p>
-                        <p>
-                            {restaurant.opening_hours.open_now ? (
-                                <p className="open">Open</p>
-                            ) : (
-                                <p className="closed">Closed</p>
-                            )}
-                        </p>
+                        <div className="img-content">
+                            <img src={restaurant.photos[0].links[0]}></img>
+                        </div>
+
+                        <div className="text-content">
+                            <h1>{restaurant.name}</h1>
+                            <p className="cuisine">
+                                Cuisine: {restaurant.cuisine}
+                            </p>
+                            <p className="rest-address">
+                                {restaurant.formatted_address}
+                            </p>
+                            <div className="delivery">
+                                <p>Delivery:</p>{" "}
+                                {restaurant.delivery ? (
+                                    <p className="status-positive">Yes</p>
+                                ) : (
+                                    <p className="status-negative">No</p>
+                                )}
+                                <p>Pickup:</p>
+                                {restaurant.pickup ? (
+                                    <p className="status-positive">Yes</p>
+                                ) : (
+                                    <p className="status-negative">No</p>
+                                )}
+                            </div>
+                            <div className="open-close">
+                                {restaurant.opening_hours.open_now ? (
+                                    <p className="status-positive">Open</p>
+                                ) : (
+                                    <p className="status-negative">Closed</p>
+                                )}
+                            </div>
+                        </div>
                     </div>
                 ))}
             </div>
