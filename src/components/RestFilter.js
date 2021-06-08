4d43ddb6631a94a764c2bc5c62857848549c1949
Ami Onodera <onodera.camila@gmail.com>
2021-06-08 21:01:17 +0200

    styling dropdown buttons - WIP

--- src/components/RestFilter.js
+++ src/components/RestFilter.js
@@ -1,19 +1,35 @@
 import react from "react";
 import RestaurantList from "./RestaurantList";
-import "./restaurantList.css";
+import "./restFilter.css";
 
 export default function RestFilter() {
     return (
-        <div>
-            <select>
-                <option>Show all</option>
-                <option>Chinese</option>
-                <option>Indian</option>
-                <option>Italian</option>
-                <option>Mexican</option>
-                <option>Thai</option>
-                <option>Turkish</option>
-            </select>
+        <div className="filters-container">
+            <div className="filter-dropdown">
+                <select className="cuisine-filter">
+                    <option>Show all</option>
+                    <option>Chinese</option>
+                    <option>Indian</option>
+                    <option>Italian</option>
+                    <option>Mexican</option>
+                    <option>Thai</option>
+                    <option>Turkish</option>
+                </select>
+            </div>
+            <div className="filter-dropdown">
+                <select className="delivery-filter">
+                    <option>Show all</option>
+                    <option>Delivery</option>
+                    <option>Pickup</option>
+                </select>
+            </div>
+            <div className="filter-dropdown">
+                <select className="open-filter">
+                    <option>Show all</option>
+                    <option>Open</option>
+                    <option>Closed</option>
+                </select>
+            </div>
         </div>
     );
 }
